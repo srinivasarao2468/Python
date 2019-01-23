7e316e62d746cc37e067787f5ce7d691124aee48
srinivasarao2468 <a.srinivasarao2468@gmail.com>
2019-01-23 07:15:10 +0530

    added rise amount method

--- classes.py
+++ classes.py
@@ -7,8 +7,15 @@ class Employee:
     def fullname(self):
         return "{} {}".format(self.fname, self.lname)
 
+    def amount_rise(self):
+        amount_increment=1.04
+        increment=self.salary*amount_increment
+        return increment
+
 emp1 = Employee("Sri", "alluri", 50000)
 emp2 = Employee("Hari", "kammana", 60000)
 
 print(emp1.fullname())
 print(emp2.fullname())
+print(emp1.amount_rise())
+print(emp2.amount_rise())
